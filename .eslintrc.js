b1c6d5c265c915e223983683cf3deecfe125cd67
Magno Fernando Brixner Biét <magno.biet@gmail.com>
2018-02-22 21:15:32 -0300

    chore: Add EditorConfig and ESLint rules

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	"env": {
+		"es6": true,
+		"node": true,
+		"browser": true
+	},
+	"globals": {},
+	"extends": [
+		"eslint:recommended"
+	],
+	"rules": {
+		"indent": ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		"no-console": ["error", {
+			"allow": ["log", "warn", "error", "debug"]
+		}],
+		"quotes": ["error", "single"],
+		"camelcase": ["error"],
+		"semi": ["error", "always"],
+	}
+};
