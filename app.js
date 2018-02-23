cd4446ce56493ed0093250ea27a196618c97691c
Magno Fernando Brixner Biét <magno.biet@gmail.com>
2018-02-22 21:51:25 -0300

    feat: Add `gzip` middleware

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const compression = require('compression');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
